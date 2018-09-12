0b8c8a3e5514ea579ffb57934b200ea99c34b991
claytonketner <claytonketner@me.com>
2018-09-11 21:29:06 -0700

    Added case for windows subsystem for linux

--- build.sh
+++ build.sh
@@ -10,10 +10,20 @@ fi
 
 FULL_FILENAME=`printf "%s%s" $BASE_FILENAME $FILENAME_SUFFIX`
 
-export TEXINPUTS="sty//:"  # Include the ./sty dir
+export TEXINPUTS="sty//:$TEXINPUTS"  # Include the ./sty dir
 pdflatex -output-format pdf -jobname $FULL_FILENAME -output-directory output resume.tex
 if [[ "$(uname)" == "Linux" ]]; then
-	xdg-open output/$FULL_FILENAME.pdf
+	if grep -qE "(Microsoft|WSL)" /proc/version &> /dev/null ; then
+		# Windows subsystem for Linux (win 10 bash)
+		# Need to copy the output to a tmp dir on the C drive so that a windows
+		# app can open it
+		mkdir -p /mnt/c/tmp
+		cp output/$FULL_FILENAME.pdf /mnt/c/tmp/
+		cmd.exe /C start `wslpath -w /mnt/c/tmp/$FULL_FILENAME.pdf`
+	else
+		# Normal Linux
+		xdg-open output/$FULL_FILENAME.pdf
+	fi
 elif [[ "$(uname)" == "Darwin" ]]; then
 	open -a Preview output/$FULL_FILENAME.pdf
 else
